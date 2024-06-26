8a176f5556468ac7d04735120959a540f54c052c
NightCrawler06 <euelv720@gmail.com>
2024-06-26 22:54:04 +0800

    index.js

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/1451001600000", (err, data) => {
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" })
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
